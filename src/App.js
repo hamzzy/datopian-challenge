a1bfdcf539470fb2e2edc8dbe4f76ad50dc3246e
ibrahim hamzat <hamat.ibrahim3@gmail.com>
2021-02-17 11:48:03 +0100

    fix data import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import * as d3 from 'd3'
 
-import Data from './daily-price.csv'
+import Data from 'csv-data/daily-price.csv';
 import LineChart from './components/Linechart.js'
 import Header from './components/Header';
-import {Container,Row,Jumbotron} from 'react-bootstrap';
+import {Container,Row,Jumbotron,Col} from 'react-bootstrap';
 
 
 const parseNumber = d => {
@@ -23,17 +23,24 @@ const App = () => {
       <Header/>
 
       <Container>
+      <Row>     
+     <Col>
         <div class="name">
            <h1>Henry Hub Natural Gas  Daily Prices Graph</h1>
         </div>
+        </Col>
+
+        </Row>
       </Container>
 
       <Container >
+        <Col>
   <Row>     
      <Jumbotron>        
     <LineChart data={data} />
         </Jumbotron>
         </Row>
+        </Col>
 </Container>
     </div>
   );
